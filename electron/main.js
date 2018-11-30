5a0bc76854f9467d2cc50a087d3a818ce2595ed0
Frederic G. Østby <frederic.g.ostby@gmail.com>
2018-11-30 13:45:25 +0100

    Added tar dependency

--- electron/main.js
+++ electron/main.js
@@ -6,8 +6,7 @@ const path              = require('path')
 const urlUtilities      = require('url')
 const isDev             = require('electron-is-dev')
 const windowStateKeeper = require('electron-window-state')
-
-const menu          = require('./ui/menu')
+const menu              = require('./ui/menu')
 
 let win
 
