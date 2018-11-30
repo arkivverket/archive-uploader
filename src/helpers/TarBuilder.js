20883ed902185dfce63149991fb130313d94a3cd
Frederic G. Østby <frederic.g.ostby@gmail.com>
2018-11-30 16:11:11 +0100

    Added comments

--- src/helpers/TarBuilder.js
+++ src/helpers/TarBuilder.js
@@ -3,6 +3,14 @@ const fs       = window.require('fs-extra')
 const path     = window.require('path')
 const tar      = window.require('tar')
 
+/**
+ * Builds a tar file from the source firectory and
+ * returns a promise with the path to the generated tar file.
+ *
+ * @param  string sourceDir Source directory
+ * @param  string tarName   Desired tar name (without extension)
+ * @return Promise
+ */
 export const buildTar = (sourceDir, tarName) => {
 	return fs.readdir(sourceDir).then((files) => {
 		let fileList = []
