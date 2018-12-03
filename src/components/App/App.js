b8970fbd9e53b84bfa34f0126e1fe917e53be31b
Frederic G. Østby <frederic.g.ostby@gmail.com>
2018-12-03 13:15:46 +0100

    Testing tar building

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from 'react'
+import { buildTar } from '../../helpers/TarBuilder'
+import './App.scss'
+
+const fs = window.require('fs-extra')
 
 class App extends Component {
+	/**
+	 *
+	 */
+	onDragEnterHandler(event) {
+		 event.stopPropagation()
+	}
+	/**
+	 *
+	 */
+	onDragOverHandler(event) {
+		event.preventDefault()
+	}
+	/**
+	 *
+	 */
+	onDropHandler(event) {
+		event.stopPropagation()
+
+		if (event.dataTransfer.files.length > 1) {
+			return alert('You can only upload a single folder!')
+		}
+
+		if(!fs.statSync(event.dataTransfer.files[0].path).isDirectory()) {
+			return alert('You must upload a folder!')
+		}
+
+		buildTar(event.dataTransfer.files[0].path, 'foobar').then((path) => {
+			alert('Tar created: ' + path)
+		})
+	}
+
+	/**
+	 *
+	 */
 	render() {
-		const electron = window.require('electron')
+		/*const electron = window.require('electron')
 
 		electron.ipcRenderer.on('start-upload', function(event, url)
 		{
 			console.log(url)
-		})
+		})*/
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+			<div className="container">
+				<div className="header">
+					Active uploads (0)
+				</div>
+				<div className="dropzone" onDragEnter={this.onDragEnterHandler} onDragOver={this.onDragOverHandler} onDrop={this.onDropHandler}>
+					Drop your folder here ...<br/><br/><br/>
+				</div>
 			</div>
 		);
 	}
