0486e4afee2ee038aea1a2660161180ef35de53e
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-11-12 15:41:19 +0100

    Bump react-router-dom from 5.2.0 to 6.0.2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { isDirectoryEmpty } from '../../helpers/fsHelpers'
 import Settings from './Settings/Settings'
 import Uploader from './Uploader/Uploader'
@@ -160,15 +160,15 @@ class App extends Component {
 
 		return (
 			<HashRouter>
-				<Switch>
-					<Route path="/settings">
-						<Settings />
-					</Route>
-					<Route path="/">
-						<Uploader addUpload={this.addUpload} upload={this.state.uploadTemplate} key={key} />
-						<Uploads uploads={this.state.uploads} removeUpload={this.removeUpload} />
-					</Route>
-				</Switch>
+				<Routes>
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/" element={
+						<>
+							<Uploader addUpload={this.addUpload} upload={this.state.uploadTemplate} key={key} />
+							<Uploads uploads={this.state.uploads} removeUpload={this.removeUpload} />
+						</>
+					} />
+				</Routes>
 			</HashRouter>
 		)
 	}
